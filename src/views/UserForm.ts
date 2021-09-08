24c9a5015e20b32623b2015c44a15584ca23821a
Dmitriy Kozhushko <dvkozhushko@sberbank.ru>
2021-09-08 16:49:36 +0300

    rerender model

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,29 +1,36 @@
+import { User } from '../models/User'
+
 export class UserForm {
 
-    constructor(public parent: Element) {
+    constructor(
+        public parent: Element, public model: User) {
+        this.bindModel()
+    }
+
+    bindModel(): void {
+        this.model.on('change', () => {
+            this.render()
+        })
     }
 
     eventsMap(): { [key: string]: () => void } {
         return {
-            'click:button': this.onButtonClick,
-            'mouseenter:h1': this.onHeaderHover
+            'click:.set-age': this.onSetAgeClick
         }
     }
 
-    onButtonClick(): void {
-        console.log('!!!')
-    }
-
-    onHeaderHover(): void {
-        console.log('h1 hover!')
+    onSetAgeClick = (): void => {
+        this.model.setRandomAge()
     }
 
     template(): string {
         return `
             <div>
                 <h1>User Form</h1>
+                <div>User name: ${this.model.get('name')}</div>
+                <div>User age: ${this.model.get('age')}</div>
                 <input />
-                <button>Click me</button>
+                <button class="set-age">Set random age</button>
             </div>
         `
     }
@@ -41,6 +48,8 @@ export class UserForm {
     }
 
     render(): void {
+        this.parent.innerHTML = ''
+
         const templateElement = document.createElement('template')
         templateElement.innerHTML = this.template()
 
