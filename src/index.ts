aed18b903dbe7fa4183279dc1b5c575f7571d31b
Dmitriy Kozhushko <dvkozhushko@sberbank.ru>
2021-09-22 10:16:13 +0300

    saving data

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { UserForm } from './views/UserForm'
 import { User } from './models/User'
 
 const user = User.buildUser({ name: 'NAME', age: 23 })
+const root = document.getElementById('root')
 
-const userForm = new UserForm(
-    document.getElementById('root'),
-    user
-)
+if (root) {
+    const userForm = new UserForm(
+        root,
+        user
+    )
 
-userForm.render()
+    userForm.render()
+} else {
+    throw new Error('Root element not found')
+}
