ef5731403a9a5e1d5a2ffb61971f5e4a61d96a5d
Dmitriy Kozhushko <dvkozhushko@sberbank.ru>
2021-10-04 18:11:22 +0300

    view nesting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { UserForm } from './views/UserForm'
+import { UserEdit } from './views/UserEdit'
 import { User } from './models/User'
 
 const user = User.buildUser({ name: 'NAME', age: 23 })
 const root = document.getElementById('root')
 
 if (root) {
-    const userForm = new UserForm(
-        root,
-        user
-    )
+    const userEdit = new UserEdit(root, user)
 
-    userForm.render()
+    userEdit.render()
+
+    console.log('userEdit', userEdit)
 } else {
     throw new Error('Root element not found')
 }
--- src/views/View.ts
+++ src/views/View.ts
@@ -50,6 +50,9 @@ export abstract class View<T extends Model<K>, K> {
         }
     }
 
+    onRender(): void {
+    }
+
     render(): void {
         this.parent.innerHTML = ''
 
@@ -58,6 +61,8 @@ export abstract class View<T extends Model<K>, K> {
 
         this.bindEvents(templateElement.content)
         this.mapRegions(templateElement.content)
+
+        this.onRender()
 
         this.parent.append(templateElement.content)
     }
